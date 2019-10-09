c29e9ade294576f8203b116711a2177266d12a54
EC2 Default User <ec2-user@ip-172-31-54-98.ec2.internal>
2019-10-09 21:48:36 +0000

    3rd commit

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-var http = require('http'),
-    fs = require('fs');
+var http = require('http');
+var fs = require('fs');
 
+const PORT=process.env.PORT; 
 
-fs.readFile('hellophaser/index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
+fs.readFile('./home.html', function (err, html) {
+
+    if (err) throw err;    
+
     http.createServer(function(request, response) {  
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(html);  
         response.end();  
-    }).listen(8000);
+    }).listen(PORT);
 });
